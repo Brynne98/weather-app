3cc78e3170d7f870ecc61155219c8309fd412410
Brynne98 <36798170+Brynne98@users.noreply.github.com>
2020-12-07 12:21:59 +0200

    Abstracted forecast method to seperate class

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
-const request = require("request");
 const geoCode = require("./geoCode")
+const forecast = require("./forecast")
 
-const WEATHER_STACK_ACCESS_KEY = "101b90eac04a2ec40f8747db6f9bf755";
-const WEATHER_STACK_BASE_URL = "http://api.weatherstack.com/current?access_key=" + WEATHER_STACK_ACCESS_KEY;
+const address = process.argv[2];
 
-// request.get({url: MAPBOX_BASE_URL, json: true}, (error, response) => {
-//     if(error) {
-//         console.log("Unable to access mapbox services.")
-//     } else if(response.body.features === undefined) {
-//        console.log("No location found for given name.")
-//     } else {
-//         let positionArr = response.body.features[0].center;
-//         const latitude = positionArr[0];
-//         const longitude = positionArr[1];
-//         console.log("Latitude: " + latitude);
-//         console.log("Longitude: " + longitude);
-//         getWeatherForLatAndLong(latitude, longitude);
-//     }
-// })
-//
-// const getWeatherForLatAndLong = (latitude, longitude) => {
-//
-//     let url = WEATHER_STACK_BASE_URL + "&query=" + latitude + "," + longitude;
-//
-//     request.get({url: url, json: true}, (error, response) => {
-//         if (error) {
-//             console.log("Unable to connect to location services.")
-//         } else if (response.body.error) {
-//             console.log("Unable to find location. Try another search.")
-//         } else {
-//             const data = response.body.current;
-//             console.log(data.weather_descriptions[0] + ". It is currently " + data.temperature + " degrees out. It feels like " + data.feelslike + " degrees out.");
-//         }
-//     })
-// }
-
-geoCode("Kempton Park", (error, data) => {
-    console.log("Error: " + error);
-    console.log("Data: " + JSON.stringify(data, null, 2));
-})
+if(!address) {
+    console.log("Please provide an address")
+} else {
+    geoCode(address, (error, data) => {
+        if(error) {
+            console.log(error)
+        } else {
+            forecast(data.latitude, data.longitude, (error, forecastData) => {
+                if(error) {
+                    console.log(error)
+                } else {
+                    console.log(data.locationName)
+                    console.log(forecastData.description + ". With a temperature of " + forecastData.temperature + "C and a feels like of " + forecastData.feelsLike + "C");
+                }
+            })
+        }
+    })
+}
